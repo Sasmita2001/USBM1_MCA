6eb70ceb365c24c1bb3afd3b3eb4fa025ecbe176
sasmita <sasmitam441@gmail.com>
2024-10-29 08:31:18 +0530

    task

---
+++ Frontend/Javascript/class-8/array.js
@@ -0,0 +1,87 @@
+//Array:
+
+//Array is a collection of elements
+
+//Ex:
+
+// let newArray = [];
+// console.log(newArray);
+
+// console.log(typeof newArray);
+
+// let name = "sasmita";
+// let age = 22;
+// let pro = "student";
+
+// let isNamed = true;
+
+//reason-1
+//We need everything under a single variable name
+//structured way,code readerly way  easy to maintain
+
+// let userArray = ["sasmita", 22, "student", true];
+// console.log(userArray);
+
+//Array declaration
+
+// let number = [1, 2, 3, 4, 5];
+// console.log(number);
+
+// let newNumber = new Array(1, 2, 3, 4, 5);
+// console.log(newNumber);
+
+//in order
+//index start in 0 /indies
+
+//How to check the length?
+//length (array(variable name).length)
+
+// let array = [3, 6, 89, 342];
+// console.log(array.length);
+
+//accesing
+
+// let cities = ["bbsr", "ctc", "jspur", "jajpur"];
+// console.log(cities[2]);
+// let lastcity = cities.length - 1;
+// console.log(cities[lastcity]);
+
+// //modify
+// console.log(cities);
+// let modifycity = "kdp";
+// cities[2] = modifycity;
+// console.log(cities);
+
+//Add element :
+//push():
+// console.log(cities);
+// let addcity = "Bhadrak";
+// cities.push(addcity);
+// console.log(cities);
+
+//Add element in an array
+// use push() method
+//element will be added in last
+
+//Remove element in an array
+//use pop()method
+// remove element in the last
+
+//pop()
+// console.log(cities);
+// cities.pop();
+// console.log(cities);
+
+//shift()
+//delete first element
+// console.log(cities);
+// cities.shift();
+// console.log(cities);
+
+//unshift()
+//add element in the first
+// console.log(cities);
+// cities.unshift("Baripada");
+// console.log(cities);
+
+//Difference the push(),pop() shift(),unshift() with proper defination and example
