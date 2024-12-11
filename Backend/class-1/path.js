3545ff89928a4ef086f22f016a542d66002453ac
sasmita <sasmitam441@gmail.com>
2024-12-11 12:39:41 +0530

    done

---
+++ Backend/class-1/path.js
@@ -0,0 +1,13 @@
+const { log } = require("console");
+const path = require("path");
+console.log(path);
+
+const rootDir = path.resolve(__dirname, "..");
+console.log(rootDir);
+
+
+//adding path 
+console.log(path.join("class-1","path.js"));
+
+console.log(path.basename());
+console.log(path.extname());
